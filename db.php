ea648c0cba2dd4b26b71cbb811b3186ab43c14f1
Davron Sherbaev <davronsherbaev@gmail.com>
2020-04-11 00:50:21 +0500

    Add db.php

---
+++ db.php
@@ -0,0 +1,57 @@
+<?php
+
+//namespace alphazet\main;
+
+class DB{
+	// Конфигурация базы данных
+     const  HOSTNAME = "localhost:3306";
+     const  DBNAME = "db_weather";
+     const  USERNAME = "root";
+     const  PASSWORD = "tiger";
+
+    //const  HOSTNAME = "localhost:3306";
+    //const  DBNAME = "iutimetable";
+    //const  USERNAME = "root";
+    //const  PASSWORD = "";
+
+    public static $db;
+    private static $j;
+
+    public static function connectDB(){
+
+        self::$db = new PDO("mysql:hostname=".self::HOSTNAME."; dbname=".self::DBNAME, self::USERNAME, self::PASSWORD);
+
+        $codirovka = self::$db->prepare("SET NAMES `utf8`");
+        $codirovka->execute();
+
+    }
+    public static function createTable($tn,$arr){
+
+        $sql = "CREATE TABLE IF NOT EXISTS `".$tn."` (";
+    foreach($arr as $k=>$v){
+
+        if($v=="int_ai") $v = "INT NOT NULL AUTO_INCREMENT UNIQUE";
+
+        $sql = $sql."`".$k."` ".$v.",";
+    }
+        $sql = $sql."PRIMARY KEY ( `id` ) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+        $create_tables = self::$db->prepare($sql);
+        $create_tables->execute();
+    }
+    public static function query($sql,$arr = null){
+        if(empty(self::$db)){
+            self::connectDB();
+        }
+        $dbres = self::$db->prepare($sql);
+        for($i=0; $i<count($arr); $i++){
+            $dbres->bindParam($i+1,self::$j[$i]);
+        }
+        for($i=0; $i<count($arr); $i++){
+            self::$j[$i] = $arr[$i];
+        }
+        $dbres->execute();
+        return  $dbres;
+    }
+}
+//    session_start();
+?>
--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 
 // include composer autoload
 require 'vendor/autoload.php';
+require './db.php';
+//
+//// import the Intervention Image Manager Class
+//use Intervention\Image\ImageManager;
+//
+//// create an image manager instance with favored driver
+//$manager = new ImageManager();
+//
+//// to finally create image instances
+//$image = $manager->make('public/minion.jpg')->resize(300, 200);
+//
+//$image->save('public/minion.2.jpg');
 
-// import the Intervention Image Manager Class
-use Intervention\Image\ImageManager;
-
-// create an image manager instance with favored driver
-$manager = new ImageManager();
-
-// to finally create image instances
-$image = $manager->make('public/minion.jpg')->resize(300, 200);
-
-$image->save('public/minion.2.jpg');
+DB::query("SELECT * FROM `User`");
 ?>
 
 <img src="public/minion.2.jpg">
